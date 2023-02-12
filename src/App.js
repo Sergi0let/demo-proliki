dc16a6e3126cc471f454aa9ba41cbc493851a58d
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-12 13:00:22 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.css';
+import './App.scss';
+
+import Logo from './common/Logo';
 
 function App() {
-  return <div className="App">Site heare Soon</div>;
+  return (
+    <div className="App">
+      <span>Site heare Soon</span>
+      <Logo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/common/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../img/logo/logo.svg';
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Logo;
