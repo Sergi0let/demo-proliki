4c371d25f16189da53115f1e521b3cca017281f8
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-12 11:19:50 +0200

    template: 0.0.1

--- src/common/Button.jsx
+++ src/common/Button.jsx
@@ -1,21 +1,7 @@
-// input component for buttons
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Button = ({ onClick, className, children }) => (
-  <button onClick={onClick} className={className} type="button">
-    {children}
-  </button>
-);
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired,
-};
-
-Button.defaultProps = {
-  className: '',
+const Button = () => {
+  return <div>Button</div>;
 };
 
 export default Button;
