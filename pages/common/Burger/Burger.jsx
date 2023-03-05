9151abfdd83f7b266018645839d5d34913707575
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-03-05 13:55:03 +0200

    -

--- pages/common/Burger/Burger.jsx
+++ pages/common/Burger/Burger.jsx
@@ -6,6 +6,33 @@ const Burger = ({ onBurger, openBurger }) => {
     onBurger(!openBurger);
   };
 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </svg>;
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      fillRule="evenodd"
+      d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+      clipRule="evenodd"
+    />
+  </svg>;
+
   return (
     <button
       className={
@@ -13,11 +40,33 @@ const Burger = ({ onBurger, openBurger }) => {
       }
       onClick={handleBurger}
     >
-      <div className={styles.block}>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-        <div className={styles.line}></div>
-      </div>
+      {openBurger ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      )}
     </button>
   );
 };
--- pages/components/HeaderFixed/HeaderFixed.jsx
+++ pages/components/HeaderFixed/HeaderFixed.jsx
@@ -6,22 +6,92 @@ import Burger from '@/pages/common/Burger/Burger';
 import Search from '@/pages/common/Search/Search';
 import List from './List/List';
 import Container from '@/pages/common/Container/Container';
+import { useRouter } from 'next/router';
 
 const HeaderFixed = ({ openBurger, onBurger, onClose }) => {
+  const { pathname } = useRouter();
+
   const burgerOpen = openBurger ? '0' : '-1000%';
 
+  const classActiveTab = {
+    borderBottom: '2px solid #3B009A',
+    paddingBottom: '16px',
+    color: '#3B009A',
+  };
   return (
     <header className={styles.header}>
-      <div className={styles.header__wrapper}>
-        <Container>
-          <h1 className={styles.header__content}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            ipsum perferendis nesciunt obcaecati repudiandae magnam. Possimus
-            porro similique ea nihil labore rerum sint commodi ipsam non
-            architecto, beatae aliquid nobis!
-          </h1>
-        </Container>
-      </div>
+      <nav className={styles.header__nav}>
+        <ul className={styles.header__top}>
+          <Link href="/" className={styles.header__image}>
+            <Image
+              src={'./img/logo/logo-l.svg'}
+              alt="Емблема сайту"
+              width={124}
+              height={32}
+              className={styles.header__logo}
+            />
+          </Link>
+          <Search />
+          <button className={styles.header__btn_see}>Для слабозорих</button>
+          <ChangeLang />
+          <Burger openBurger={openBurger} onBurger={onBurger} />
+        </ul>
+        <ul className={styles.header__menu}>
+          <li>
+            <Link
+              style={pathname === '/atx_classification' ? classActiveTab : {}}
+              href="/atx_classification"
+            >
+              ATХ класифікація
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={pathname === '/active_substances' ? classActiveTab : {}}
+              href="/active_substances"
+            >
+              Активні речовини
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={pathname === '/alphabet' ? classActiveTab : {}}
+              href="/alphabet"
+            >
+              Алфавітний покажчик
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={pathname === '/manufacturers' ? classActiveTab : {}}
+              href="/manufacturers"
+            >
+              Виробники
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={pathname === '/news' ? classActiveTab : {}}
+              href="/news"
+            >
+              Новини
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={pathname === '/about' ? classActiveTab : {}}
+              href="/about"
+            >
+              Про нас
+            </Link>
+          </li>
+          <li>
+            <Link style={pathname === '/' ? classActiveTab : {}} href="/">
+              Контакти
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
     // <header className={styles.header}>
     //   <Container>
