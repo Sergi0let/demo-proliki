866583947c479904da76a48812f00ff9bea27a64
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-03-12 11:29:23 +0200

    ActiveSubstances

--- src/components/ActiveSubstances/ActiveSubstances.jsx
+++ src/components/ActiveSubstances/ActiveSubstances.jsx
@@ -1,20 +1,28 @@
 import Container from '@/src/common/Container/Container';
 import Letter from '@/src/common/Letter/Letter';
 import RadioButton from '@/src/common/RadioButton/RadioButton';
-import { TitleBig } from '@/src/common/Title/Title';
+import Title, { TitleBig, TittleIntermidiate } from '@/src/common/Title/Title';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import active_substances from '../../data/active_substances';
 
 import styles from './ActiveSubstances.module.scss';
 
 const ActiveSubstances = () => {
   const [currentLetter, setCurrentLetter] = useState('А');
+  const [substances, setSubstances] = useState([]); // active_substances
   console.log('currentLetter', currentLetter);
+  console.log('substances', substances);
   const [currentTab, setCurrentTab] = useState('1');
   const [checked, setChecked] = useState({
     абв: true,
     abc: false,
   });
+
+  useEffect(() => {
+    const filtered = active_substances[currentLetter];
+    setSubstances(filtered);
+  }, [currentLetter, substances]);
 
   const filter = Object.keys(checked)
     .filter((key) => checked[key])
@@ -138,7 +146,11 @@ const ActiveSubstances = () => {
                   onClick={() => setCurrentLetter(elem)}
                   style={
                     currentLetter === elem
-                      ? { color: 'white', background: '#4D5156' }
+                      ? {
+                          color: 'white',
+                          background: '#4D5156',
+                          cursor: 'not-allowed',
+                        }
                       : {}
                   }
                   key={i}
@@ -147,73 +159,165 @@ const ActiveSubstances = () => {
                 </Letter>
               ))}
           </div>
+          <ul className={styles.content__links}>
+            <li>
+              <span>АБ</span>
+            </li>
+            <li>
+              <span>АВ</span>
+            </li>
+            <li>
+              <span>АГ</span>
+            </li>
+            <li>
+              <span>АД</span>
+            </li>
+            <li>
+              <span>АЕ</span>
+            </li>
+            <li>
+              <span>АЗ</span>
+            </li>
+            <li>
+              <span>АИ</span>
+            </li>
+            <li>
+              <span>АЙ</span>
+            </li>
+            <li>
+              <span>АК</span>
+            </li>
+            <li>
+              <span>АЛ</span>
+            </li>
+            <li>
+              <span>АМ</span>
+            </li>
+            <li>
+              <span>АН</span>
+            </li>
+            <li>
+              <span>АП</span>
+            </li>
+            <li>
+              <span>АР</span>
+            </li>
+            <li>
+              <span>АС</span>
+            </li>
+            <li>
+              <span>АТ</span>
+            </li>
+            <li>
+              <span>АУ</span>
+            </li>
+            <li>
+              <span>АФ</span>
+            </li>
+            <li>
+              <span>АХ</span>
+            </li>
+            <li>
+              <span>АЦ</span>
+            </li>
+            <li>
+              <span>АЧ</span>
+            </li>
+            <li>
+              <span>АШ</span>
+            </li>
+            <li>
+              <span>АЄ</span>
+            </li>
+          </ul>
+        </div>
+        <ul className={styles['active-substances__list']}>
+          {substances &&
+            substances.map((item, i) => (
+              <li key={i}>
+                <a href="#">
+                  {item.ua} <span>({item.eng})</span>
+                </a>
+              </li>
+            ))}
+        </ul>
+
+        <Title title="Активные вещества" />
+        <div className={styles['active-substances__block-text']}>
+          <p>
+            Правильное питание и физическая активность – залог здоровья,
+            хорошего настроения, заряда бодрости и отличного внешнего вида.
+            Сбалансированный рацион, даже если он составлен по всем правилам, в
+            соответствии со всеми рекомендациями относительно соотношения
+            белки/жиры/углеводы, не способен насытить организм необходимыми
+            витаминами, минералами, микроэлементами. В связи с этим, многие, кто
+            следит за здоровьем и старается поддерживать его на постоянно
+            высоком уровне, предпочитает недорого и быстро купить бады в
+            интернет аптеке.
+          </p>
+        </div>
+        <TittleIntermidiate title="Назначение и виды активных веществ" />
+        <div className={styles['active-substances__block-text']}>
+          <p>
+            Активные пищевые добавки бады – природные вещества (иногда
+            идентичные таковым), употребляемые вместе с едой, вводимые в
+            некоторые продукты в качестве дополнительного элемента в составе.
+            Основная задача – стать еще одним источником пищевых, биологически
+            активных элементов, которые налаживают работу обмена веществ. Помимо
+            этого, их употребление способно нормализировать функционирование
+            систем и органов. Не являются лекарственным средством, поэтому не
+            помогают избавиться от недугов. Но это вовсе не означает, что
+            биодобавки можно употреблять бесконтрольно, без предварительной
+            беседы с врачом.
+          </p>
+          <p>
+            На полках супермаркетов можно найти все, что угодно. Но обращая
+            внимание на состав продуктов, можно заметить, что сбалансированность
+            далека от идеала. Пищевые добавки значительно улучшили вкус, но
+            снизили содержание витаминов, микроэлементов. Именно бад препараты
+            призваны решить эту проблему, результативно справляясь с
+            восполнением нехватки.
+          </p>
+        </div>
+        <TittleIntermidiate title="Преимущества активных веществ" />
+        <div className={styles['active-substances__block-text']}>
+          <p>
+            Санитарные нормы и правила четко определяют назначение такой группы,
+            как натуральные бады. Они способны оптимизировать, привести в
+            порядок протеиновый, липидный, углеводный и другие виды обмена
+            веществ. Начинать прием рекомендовано при различных функциональных
+            расстройствах. С учетом показаний, особенностей клинической картины,
+            можно купить бады, которые значительно улучшать функциональность
+            систем организма, положительно сказаться на общем его состоянии.
+          </p>
+          <p>К основным преимуществам употребления можно отнести:</p>
+          <p className={styles['classification__block-text_list']}>
+            <span></span>Повышение иммунитета;
+          </p>
+          <p className={styles['classification__block-text_list']}>
+            <span>•</span>Восстановление микрофлоры кишечника;
+          </p>
+          <p className={styles['classification__block-text_list']}>
+            <span>•</span>Ускорение процесса выздоровления;
+          </p>
+          <p className={styles['classification__block-text_list']}>
+            <span>•</span>Снижение веса;
+          </p>
+        </div>
+        <TittleIntermidiate title="Особенности применения" />
+        <div className={styles['active-substances__block-text']}>
+          <p>
+            Несмотря на то, что добавки не являются лекарственными средствами,
+            рекомендуется придерживаться определенных правил приема. Изначально,
+            прежде чем отправится в аптеку и купить биодобавки необходимо
+            проконсультироваться с квалифицированным специалистом. Прием
+            начинается с небольших дозировок, для объективной оценки реакции на
+            препарат и постепенно доводится до рекомендованной суточной нормы.
+            Для лучшего усвоения, принимаются во время приема пищи, если иное не
+            указано в инструкции.
+          </p>
         </div>
       </div>
-      <div>ActiveSubstances</div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia maxime id
-      nemo error, eligendi voluptatibus! Placeat aspernatur molestias nemo neque
-      delectus. Sunt incidunt voluptatibus quos nam accusamus neque esse nobis?
-      Velit doloribus obcaecati recusandae, consectetur possimus quia sit,
-      accusamus rem quas necessitatibus nobis minus impedit incidunt quod
-      tenetur itaque libero maxime ad est porro. Modi laboriosam est laborum
-      sunt odio! Ut laborum iusto sint? Neque laboriosam vero recusandae
-      molestias quaerat quod facere sunt doloremque voluptatem dolores? Maiores
-      placeat labore corporis illum amet dolore nihil dicta distinctio,
-      assumenda autem vero ea. Suscipit molestias delectus ipsum beatae et,
-      voluptatem temporibus sed labore, assumenda cumque quo odit veritatis
-      mollitia quia tenetur, exercitationem dolorem. Nihil cum optio obcaecati,
-      nemo est ea quam nesciunt amet! Cum animi blanditiis, non asperiores quae
-      debitis consectetur quis repudiandae, ea rerum nemo, praesentium dolorum
-      itaque ipsum dicta vel? Molestias exercitationem, dolor quam dicta
-      officiis consequuntur aliquam iure perferendis ad. Placeat quisquam
-      incidunt vitae culpa provident ut molestias, iusto, veniam nam
-      perspiciatis deleniti laboriosam deserunt nemo tempora eaque iste. Harum
-      quisquam reprehenderit quidem dignissimos ullam reiciendis ipsa natus
-      voluptatem culpa? Recusandae mollitia atque fuga delectus cumque dicta at
-      quod quisquam consectetur provident excepturi laboriosam, magnam nesciunt
-      sunt perferendis voluptate ratione voluptates. Commodi facilis officiis
-      laborum porro optio. Iure, iste commodi! Officiis suscipit veniam quod
-      quos nulla rerum voluptatem beatae fugiat dolore obcaecati? Sunt porro
-      quisquam, iure aspernatur enim nam ratione, magni praesentium voluptates
-      facilis repudiandae aut, voluptatum excepturi autem temporibus. Suscipit
-      laborum voluptates rem, debitis, consequuntur odit adipisci accusamus
-      velit fugit ratione qui laudantium, officia nisi neque porro? Recusandae
-      harum quas provident similique ullam nisi aut reprehenderit deserunt
-      voluptatibus officiis! Mollitia voluptates animi quod laborum tenetur iste
-      magni illum ipsam, aperiam saepe iusto atque suscipit est, sunt nesciunt,
-      tempore officiis veritatis optio nobis voluptatum adipisci eligendi
-      blanditiis. Perferendis, suscipit magnam? Assumenda commodi eveniet modi
-      voluptate deserunt a officia non earum id, doloremque ipsum hic natus at
-      est. Commodi dolorum consequuntur quae porro soluta. Praesentium fugiat
-      aspernatur delectus, iste itaque quod? Nesciunt eum accusantium vel omnis
-      voluptatum nemo reprehenderit harum. Quod ea enim expedita corrupti qui
-      error, repellendus numquam omnis sapiente tempora perferendis consequuntur
-      accusamus, quo cupiditate at nemo quis. Commodi! Atque magni eos
-      exercitationem fuga voluptatem, accusamus provident minus, consequatur
-      corporis ea possimus doloribus asperiores eligendi architecto, omnis
-      voluptatibus accusantium? Deleniti quae libero consequuntur aspernatur
-      dolorem, corporis accusamus sint quas! Cupiditate dolore quaerat
-      assumenda? Reprehenderit corporis illo impedit recusandae modi dolores,
-      itaque inventore nam, nulla alias ex. Aut, minima illo vel itaque iure
-      eligendi quidem omnis, in corrupti, praesentium necessitatibus. Distinctio
-      placeat vero quam ullam quod aut totam est harum voluptas modi eius illum
-      magni ut fuga corporis error quo alias fugit obcaecati officiis, dolores
-      neque. Error blanditiis placeat dolor? Suscipit ducimus qui amet culpa
-      omnis provident impedit nemo enim quas rem nisi ipsa quasi aliquid ex
-      pariatur reiciendis perferendis reprehenderit eligendi nobis, nostrum et
-      in? Repellat quas sit voluptas! Odit atque cum iusto accusantium
-      distinctio, ratione dolore optio exercitationem reiciendis nemo delectus
-      dolorem facere, qui numquam autem. Aperiam ratione adipisci numquam
-      repudiandae iste itaque rerum sunt, molestias quas quasi! Hic rerum
-      praesentium voluptas sint, quod magni ea explicabo sit dolor ratione
-      corporis saepe dolorum corrupti alias mollitia. Iure harum quia facere.
-      Maxime ratione aut nisi ab dignissimos ut in? Quaerat ipsa atque
-      voluptatem ex accusamus animi cum sapiente eum ea blanditiis dolores,
-      magni in exercitationem eveniet at commodi tempora sequi vero eius
-      laboriosam saepe. Tempore blanditiis quos corporis tempora. Dolor deserunt
-      expedita soluta sequi quia vitae eum beatae quas nostrum. Consectetur sed
-      vitae reprehenderit consequuntur, reiciendis ut voluptates officia ipsa
-      alias expedita corrupti a mollitia aperiam nisi suscipit consequatur.
     </Container>
   );
 };
