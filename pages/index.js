ff3210bfed78e3ad2422348bf74edfaf9e3758d9
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-04-05 12:50:55 +0300

    +2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import AlphabetSection from '@/src/components/AlphabetSection/AlphabetSection';
 import Banner from '@/src/components/Banner/Banner';
-import CategorySection from '@/src/components/CategorySection/CategorySection';
 import FarmGroups from '@/src/components/FarmGroups/FarmGroups';
+import AlphabetSection from '@/src/components/AlphabetSection/AlphabetSection';
+import CategorySection from '@/src/components/CategorySection/CategorySection';
 import NewsBlock from '@/src/components/NewsBlock/NewsBlock';
 
 export default function Home() {
