1394091153cf90fb90ccab2665d9009d7c7827f3
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-04-05 12:54:57 +0300

    -1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Banner from '@/src/components/Banner/Banner';
-import FarmGroups from '@/src/components/FarmGroups/FarmGroups';
+import FarmGroups from '@/src/components/FarmGroups/FarmGroups.jsx';
 import AlphabetSection from '@/src/components/AlphabetSection/AlphabetSection';
 import CategorySection from '@/src/components/CategorySection/CategorySection';
 import NewsBlock from '@/src/components/NewsBlock/NewsBlock';
