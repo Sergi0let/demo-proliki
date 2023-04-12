96200510fcd22e5bbe4039eafcb3fb2e09b36272
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-04-12 23:01:23 +0300

    add FarmBlock component

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Banner from '@/src/components/Banner/Banner';
 import AlphabetSection from '@/src/components/AlphabetSection/AlphabetSection';
 import CategorySection from '@/src/components/CategorySection/CategorySection';
 import NewsBlock from '@/src/components/NewsBlock/NewsBlock';
-import FarmGroups from '@/src/components/FarmGroups';
+import FarmBlock from '@/src/components/FarmBlock';
+// import FarmGroups from '@/src/components/FarmGroups';
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <FarmGroups />
+      {/* <FarmGroups /> */}
+      <FarmBlock />
       <AlphabetSection />
       <NewsBlock />
       <CategorySection />
